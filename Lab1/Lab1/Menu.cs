c6c8cc1fe6122b14434c24be66cea30f45079036
Samurai622 <vovik13on@gmail.com>
2026-02-23 15:48:38 +0200

    Add Menu

---
+++ Lab1/Lab1/Menu.cs
@@ -0,0 +1,65 @@
+using System;
+
+namespace Lab1
+{
+    public static class Menu
+    {
+        public static void Start()
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Lab1 - Delegates");
+                Console.WriteLine("1. Task 1");
+                Console.WriteLine("2. Task 2");
+                Console.WriteLine("3. Task 3");
+                Console.WriteLine("4. Task 4");
+                Console.WriteLine("5. Task 5");
+                Console.WriteLine("6. Task 6");
+                Console.WriteLine("0. Вихід");
+
+                Console.Write("\nВаш вибір: ");
+                string input = Console.ReadLine();
+
+                Console.Clear();
+
+                switch (input)
+                {
+                    case "1":
+                        //Task1.Run();
+                        break;
+
+                    case "2":
+                        //Task2.Run();
+                        break;
+
+                    case "3":
+                        //Task3.Run();
+                        break;
+
+                    case "4":
+                        //Task4.Run();
+                        break;
+
+                    case "5":
+                        //Task5.Run();
+                        break;
+
+                    case "6":
+                        //Task6.Run();
+                        break;
+
+                    case "0":
+                        return;
+
+                    default:
+                        Console.WriteLine("Невірний вибір!");
+                        break;
+                }
+
+                Console.WriteLine("\nНатисніть Enter...");
+                Console.ReadLine();
+            }
+        }
+    }
+}
--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Menu.Start();
+        }
+    }
+}
