29ae3658c00e5cf34a52b7bd1c204b405c8ce5e1
Janlast <foxyvdov@gmail.com>
2026-02-23 16:21:10 +0200

    Added class Timer

---
+++ Lab1/Lab1/Tasks/Task1/Timer.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Threading;
+
+namespace Lab1.Tasks
+{
+    public class Timer
+    {
+        public delegate void TimerAction();
+        private TimerAction _action;    
+        private int _interval;
+        private bool _isRunning;
+
+        public Timer(TimerAction action, int interval)
+        {
+            _action = action;
+            _interval = interval;
+            _isRunning = false;
+        }
+        public void Start()
+        {
+            if (_isRunning)
+                return;
+            _isRunning = true;
+            Thread timerThread = new Thread(() =>
+            {
+                while (_isRunning)
+                {
+                    Thread.Sleep(_interval * 1000);
+                    if (_isRunning)
+                        _action?.Invoke();
+                }
+            });    
+            timerThread.IsBackground = true;
+            timerThread.Start();        
+        }
+        public void Stop()
+        {
+            _isRunning = false;
+        }
+    }
+}
