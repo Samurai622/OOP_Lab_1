f456e59b9c50637783330db48ac6353e448d4676
Samurai622 <vovik13on@gmail.com>
2026-02-23 18:52:29 +0200

    Added menu starting stage

--- Lab1/Lab1_Console/Tasks/Task2/Task2.cs
+++ Lab1/Lab1_Console/Tasks/Task2/Task2.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 
 namespace Lab1
 {
@@ -46,6 +47,58 @@ namespace Lab1
                         break;
                 }
                 Console.WriteLine($"\nПочатковий масив: [ {string.Join(", ", originalArray)} ]");
+
+                int k;
+                while(true)
+                {
+                    Console.Write("\nEnter k (number of elements to move): ");
+                    if(int.TryParse(Console.ReadLine(), out k) && k != 0)
+                        break;
+                    Console.WriteLine("Invalid input. Please enter a non-zero integer.");
+                }
+                FilterDelegate isMultipleOfK = (number) => number % k == 0;
+
+                bool isRunning = true;
+                while (isRunning)
+                {
+                    Console.WriteLine("\n=== STAGE 2: MENU STARTING STAGE ===");
+                    Console.WriteLine("1 - Method A: using Enumerable.Where");
+                    Console.WriteLine("2 - Method B: using custom loop");
+                    Console.WriteLine("3 - Using both methods and comparing results");
+                    Console.WriteLine("0 - Exit");
+                    Console.Write("Your choice: ");
+
+                    string actionChoice = Console.ReadLine();
+                    Console.WriteLine();
+
+                    switch (actionChoice)
+                    {
+                        case "1":
+                            int[] resWhere = ArrayProcessor.FilterWithWhere(originalArray, isMultipleOfK);
+                            Console.WriteLine($"Result using method A: [ {string.Join(", ", resWhere)} ]");
+                            break;
+                        case "2":
+                            int[] resCustom = ArrayProcessor.FilterCustom(originalArray, isMultipleOfK);
+                            Console.WriteLine($"Result using method B: [ {string.Join(", ", resCustom)} ]");
+                            break;
+                        case "3":
+                            int[] arrayA = ArrayProcessor.FilterWithWhere(originalArray, isMultipleOfK);
+                            int[] arrayB = ArrayProcessor.FilterCustom(originalArray, isMultipleOfK);
+                            Console.WriteLine($"Result using method A: [ {string.Join(", ", arrayA)} ]");
+                            Console.WriteLine($"Result using method B: [ {string.Join(", ", arrayB)} ]");
+                            
+                            bool areEqual = arrayA.SequenceEqual(arrayB);
+                            Console.WriteLine($"Are results equal? {(areEqual ? "Yes" : "No")}");
+                            break;
+                        case "0":
+                            Console.WriteLine("Exiting...");
+                            isRunning = false;
+                            break;
+                        default:
+                            Console.WriteLine("Invalid choice!");
+                            break;
+                    }
+                }
             }
         }
     }
