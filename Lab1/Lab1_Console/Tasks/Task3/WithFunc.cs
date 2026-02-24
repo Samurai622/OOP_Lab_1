d7cb77585ca7e474c819caf4d10c9426869dda92
Samurai622 <vovik13on@gmail.com>
2026-02-24 15:50:33 +0200

    Added class WithFunc

---
+++ Lab1/Lab1_Console/Tasks/Task3/WithFunc.cs
@@ -0,0 +1,26 @@
+using System;
+namespace Lab1
+{
+    public class WithFunc
+    {
+        public static double Compute(double precision, Func<int, double> getTerm)
+        {
+            double sum = 0;
+            int n = 0;
+            double currentTerm;
+
+            do
+            {
+                currentTerm = getTerm(n);
+                if(Math.Abs(currentTeam) >= precision)
+                {
+                    sum += currentTerm;
+                }
+                n++;
+            }
+            while (Math.Abs(currentTerm) >= precision);
+
+            return sum;
+        }
+    }
+}
