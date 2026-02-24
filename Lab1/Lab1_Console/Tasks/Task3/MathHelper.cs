5ff4f25623e60850044271cb877b68f5ce232e4e
Samurai622 <vovik13on@gmail.com>
2026-02-24 16:10:17 +0200

    Added "Formulas" in MathHelper.cs

--- Lab1/Lab1_Console/Tasks/Task3/MathHelper.cs
+++ Lab1/Lab1_Console/Tasks/Task3/MathHelper.cs
@@ -1,13 +1,28 @@
 using System;
 namespace Lab1
 {
-    public class MathHelper
+    public static class MathHelper
     {
         public static double FactorialInverse(int n)
         {
             double fact = 1;
             for(int i = 1; i <= n; i++) fact *= i;
             return 1.0 / fact;
+        }
+
+        public static double Formula_1(int n)
+        {
+            return 1.0 / Math.Pow(2, n);
+        }
+
+        public static double Formula_2(int n)
+        {
+            return FactorialInverse(n);
+        }
+
+        public static double Formula_3(int n)
+        {
+            return 1.0 / Math.Pow(-0.5, n);
         }
     }
 }
