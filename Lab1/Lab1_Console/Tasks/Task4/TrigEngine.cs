74e559409f1752e71a0bb6aa06a7b5d9b73054e6
Samurai622 <vovik13on@gmail.com>
2026-03-03 13:37:28 +0200

    Created and added logic task4

---
+++ Lab1/Lab1_Console/Tasks/Task4/TrigEngine.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace Lab1
+{
+    public static class TrigEngine
+    {
+        public static readonly string[] Names =
+        {
+            "sin",
+            "cos",
+            "tan",
+            "cot",
+            "exit"
+        };
+
+        public static readonly Func<double, double>[] Operations =
+        {
+            x => Math.Sin(x),
+            x => Math.Cos(x),
+            x => Math.Tan(x),
+            x => 1 / Math.Tan(x),
+            x => throw new InvalidOperationException("Exit")
+        };
+
+        public static double Execute(string op, double value)
+        {
+            int index = Array.IndexOf(Names, op);
+            return Operations[index](value);
+        }
+    }
+}
